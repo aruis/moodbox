e1fd51ce7d32a518a2e4c0876529e235690c24e4
liurui <lovearuis@gmail.com>
2023-03-13 22:16:14 +0800

    Initial Commit

---
+++ moodbox/moodboxApp.swift
@@ -0,0 +1,20 @@
+//
+//  moodboxApp.swift
+//  moodbox
+//
+//  Created by 牧云踏歌 on 2023/3/13.
+//
+
+import SwiftUI
+
+@main
+struct moodboxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
